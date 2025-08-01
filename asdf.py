089eae1a68cd4f1d61ea28e91eae3ded7b37de14
Mridul Verma <mridul_verma@hotmail.com>
2025-08-01 15:15:49 -0700

    commit

--- asdf.py
+++ asdf.py
@@ -1,7 +1,9 @@
-Header: I went to PAX West in 2011, my first ever convention
+Header
 
 Edit 2: I am replacing the words "Edit 1" with "Header"
 
 Edit 3: In this edit I deleted the original header
 
 Edit 4: I have switched Edits 2 and 3
+
+Edit 5: I am deleting the text in "Header"
